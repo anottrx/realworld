23fbd18dccbc522b135eff13db1dcf5513825847
ryuna <anottrx@gmail.com>
2022-12-04 04:09:25 +0900

    chore: storybook 설정 (#1)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ['airbnb', 'next/core-web-vitals'],
+  extends: ['airbnb', 'next/core-web-vitals', 'plugin:storybook/recommended'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-  },
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }]
+  }
 };
