1ce0b08bcc62af7c3ab5e7ba521cc54ed238784b
ryuna <anottrx@gmail.com>
2022-12-05 21:08:38 +0900

    design: 폰트 설정 (#3)

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <link rel="stylesheet" as="style" crossOrigin="true" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
