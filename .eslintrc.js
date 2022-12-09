458173f0dda777c3d41e3536beb69aeb6d838957
ryuna <anottrx@gmail.com>
2022-12-09 18:34:54 +0900

    chore: 린트 환경 설정 (#7)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es2021: true,
   },
   extends: ['airbnb', 'next/core-web-vitals', 'plugin:storybook/recommended'],
+  ignorePatterns: '@components/**/*.stories.tsx',
   rules: {
     'react/jsx-filename-extension': [
       1,
@@ -13,5 +14,15 @@ module.exports = {
     ],
     'react/function-component-definition': 'off',
     'react/jsx-props-no-spreading': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
 };
